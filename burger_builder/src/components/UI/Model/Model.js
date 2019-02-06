59029ff191d4bbc4b2e90e02a11dd4d24d80922f
Darshan Malaviya <darshan036547@gmail.com>
2019-02-06 10:27:52 -0700

    added sidebar for mobile devices and changed folder structure

--- burger_builder/src/components/UI/Model/Model.js
+++ burger_builder/src/components/UI/Model/Model.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Model.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
-const model = (props) => {
-    return (
-        <Aux>
-            <Backdrop show={props.show} clicked={props.modalClosed}/>
-            <div className={classes.Modal} 
-                        style={{ 
-                                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)'
-                                , opacity : props.show ? '1' : '0'
-                                }}
-                        >
-            {props.children}
-        </div>
-        </Aux>
-    );
+
+class Model extends Component {
+    shouldComponentUpdate(nextProps,nextState){
+        if(nextProps.show !== this.props.show){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    componentWillUpdate(){
+        console.log('[ModelWillUpdate]')
+    }
+    
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)'
+                        , opacity: this.props.show ? '1' : '0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+
+    }
 }
- 
-export default model;
+
+
+export default Model;
